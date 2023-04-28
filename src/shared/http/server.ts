45bcda504afab9f9abc9e18ad5d57e58f5d8d57b
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-28 10:33:09 -0300

    first server config

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello Dev!");
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const PORT = process.env.PORT || 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}! 🤩`);
+});
