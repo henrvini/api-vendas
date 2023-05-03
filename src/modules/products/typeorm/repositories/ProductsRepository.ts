644c1008932ce67cce7aeb65d086c3c2a9ef563a
Vinicius Rodrigues <vini0593@gmail.com>
2023-05-03 15:08:14 -0300

    ProductsRepository and CreateProductService

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from "typeorm";
 import Product from "../entities/Product";
 
 @EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {
+export class ProductsRepository extends Repository<Product> {
   public async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({
       where: {
@@ -14,4 +14,4 @@ export class ProductRepository extends Repository<Product> {
   }
 }
 
-export default ProductRepository;
+export default ProductsRepository;
