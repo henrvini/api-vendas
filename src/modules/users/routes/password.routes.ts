d62c5c7f4430a824295c2cbe9a035fd8c4d75d0a
Vinicius Rodrigues <vini0593@gmail.com>
2023-05-16 13:43:11 -0300

    ResetPasswordService and updated password.routes

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate";
 import ForgotPasswordController from "../controllers/ForgotPasswordController";
+import ResetPasswordController from "../controllers/ResetPasswordController";
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post(
   "/forgot",
@@ -15,5 +17,16 @@ passwordRouter.post(
   forgotPasswordController.create,
 );
 
+passwordRouter.post(
+  "/reset",
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref("password")),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRouter;
