ef8d4947c059f060f16916c8c3d8ce59a81136b5
Vinicius Rodrigues <vini0593@gmail.com>
2023-05-16 16:09:31 -0300

    Ethereal Email, nodemail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import EtherealMail from "@config/mail/EtherealMail";
 
 interface IRequest {
   email: string;
@@ -18,9 +19,12 @@ class SendForgotPasswordEmailService {
       throw new AppError("User does not exists");
     }
 
-    const token = await userTokenRepository.generate(user.id);
+    const { token } = (await userTokenRepository.generate(user.id)) ?? {};
 
-    console.log(token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token}`,
+    });
   }
 }
 
