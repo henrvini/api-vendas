2a6a13631b7e4d2666d8a6fbdd73c8c70eb98fae
Vinicius Rodrigues <vini0593@gmail.com>
2023-05-23 17:47:25 -0300

    OrdersProducts entity and added new relations on others entities

---
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import Order from "./Order";
+import Product from "@modules/products/typeorm/entities/Product";
+
+@Entity("orders_products")
+class OrderProducts {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: "order_id" })
+  order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: "product_id" })
+  product: Product;
+
+  @Column()
+  price: number;
+
+  @Column()
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default OrderProducts;
