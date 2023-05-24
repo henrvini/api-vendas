988b8901af721786c19e3dfac68b54f99ee53ed5
Vinicius Rodrigues <vini0593@gmail.com>
2023-05-24 16:31:28 -0300

    class-transformer on password and avatar of user

--- src/modules/users/controllers/UserAvatarController.ts
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 import AppError from "@shared/errors/AppError";
+import { instanceToInstance } from "class-transformer";
 
 export default class UserAvatarController {
   public async update(req: Request, res: Response): Promise<Response> {
@@ -15,6 +16,6 @@ export default class UserAvatarController {
       avatarFilename: req.file.filename,
     });
 
-    return res.json(user);
+    return res.json(instanceToInstance(user));
   }
 }
